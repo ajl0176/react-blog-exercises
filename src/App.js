f0a3d9bb6a3975ceef527409e30815cbd9ec6bab
A. James Lindsey <macbookair@MacBooks-MacBook-Air.local>
2020-09-30 20:15:52 -0400

    Part A Done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class BlogPost extends Component {
+  render() {
+      return (
+        <form className ="col-12 mb-5">
 
-export default App;
+          <div className="form-group">
+            <label for="exampleFormControlTextarea1">Blog Title and Body</label>
+            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          </div>
+        </form>
+    )
+  }
+};
+
+export default BlogPost
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BlogPost from './App';
 import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BlogPost />
   </React.StrictMode>,
   document.getElementById('root')
 );
